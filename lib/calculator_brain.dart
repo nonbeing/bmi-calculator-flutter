ddc954590a10618f09036f38614ecac1d8ea1a91
nonbeing <anattaspace@gmail.com>
2019-07-19 11:10:46 +0530

    Improvements

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -4,6 +4,8 @@ class CalculatorBrain {
   final int height;
   final int weight;
   double _bmi;
+  String _idealWeight1;
+  String _idealWeight2;
 
   CalculatorBrain({this.height, this.weight});
 
@@ -12,29 +14,37 @@ class CalculatorBrain {
     return _bmi.toStringAsFixed(1);
   }
 
+  void calculateIdealWeightRange() {
+    _idealWeight1 = (pow(height / 100, 2) * 18).toStringAsFixed(0);
+    _idealWeight2 = (pow(height / 100, 2) * 25).toStringAsFixed(0);
+  }
+
   Map<String, String> getResults() {
+    calculateIdealWeightRange();
+
     if (_bmi > 30)
       return {
         'result': 'Obese',
         'interpretation':
-            'You have a higher body weight than normal, and at an increased risk of associated health problems. Try to lose weight with suitable diet and exercise.'
+            'Your body weight ($weight kg) is higher than normal. \n\nThe ideal weight range for your height is: $_idealWeight1 kg - $_idealWeight2 kg. \n\n You are at an increased risk of certain health problems. Try to lose weight with suitable diet and exercise.'
       };
     else if (_bmi > 25 && _bmi < 30)
       return {
         'result': 'Overweight',
         'interpretation':
-            'You have a higher body weight than normal. Try to lose some weight with suitable diet and exercise.'
+            'Your body weight ($weight kg) is higher than normal. \n\nThe ideal weight range for your height is: $_idealWeight1 kg - $_idealWeight2 kg.'
       };
     else if (_bmi > 18.5 && _bmi < 25)
       return {
         'result': 'Normal',
-        'interpretation': 'You have a normal body weight. Good job!'
+        'interpretation':
+            'Your body weight ($weight kg) is in the ideal range for your height: $_idealWeight1 kg - $_idealWeight2 kg. \n\nGood job!'
       };
     else
       return {
         'result': 'Underweight',
         'interpretation':
-            'You have a lower body weight than normal. Try to gain some weight with suitable diet and exercise.'
+            'Your body weight ($weight kg) is lower than normal. \n\nThe ideal weight range for your height is: $_idealWeight1 kg - $_idealWeight2 kg.'
       };
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/screens/input_page.dart';
 import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: ThemeData(
         primaryColor: mainThemeColor,
